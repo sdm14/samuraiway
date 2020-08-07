dea50e03a8fd65eec8c40451421d21a3ba1c28d4
sdm14 <149sdm10@gmail.com>
2020-08-07 22:16:01 +0600

    61 урок. Cookie, login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import './App.css'
-import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar'
 import UsersContainer from './components/Users/UsersContainer'
 import { Route } from 'react-router-dom'
 import DialogsContainer from './components/Dialogs/DialogsContainer'
 import ProfileContainer from './components/Profile/ProfileContainer'
+import HeaderContainer from './components/Header/HeaderContainer'
 
 function App(props) {
   return (
     <div className="app-wrapper">
-      <Header />
+      <HeaderContainer />
       <Navbar />
       <div className="app-wrapper-content">
         <Route path="/profile/:userId?" render={() => <ProfileContainer />} />
--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -1,12 +1,14 @@
 import { dialogsReducer } from './dialogsReducer'
 import { profileReducer } from './profileReducer'
 import { usersReducer } from './usersReducer'
+import { authReducer } from './authReducer';
 const { createStore, combineReducers } = require("redux");
 
 const reducers = combineReducers({
    profilePage: profileReducer,
    dialogsPage: dialogsReducer,
-   usersPage: usersReducer
+   usersPage: usersReducer,
+   auth: authReducer
 })
 
 let store = createStore(reducers)
